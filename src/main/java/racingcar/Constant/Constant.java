d0b4cdff9d8ac546983c28e916733ef40e40aa69
EUNJEONGMUN <munej26@gmail.com>
2022-11-26 15:03:04 +0900

    feat: 구분자 상수 추가

--- src/main/java/racingcar/Constant/Constant.java
+++ src/main/java/racingcar/Constant/Constant.java
@@ -2,4 +2,7 @@ package racingcar.Constant;
 
 public class Constant {
     public static final int EMPTY_SIZE = 0;
+    public static final String DELIMITER = " : ";
+    public static final String MOVE_MARK =  "-";
+    public static final String ENTER = "\n";
 }
