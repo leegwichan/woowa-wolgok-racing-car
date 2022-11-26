4e7918c08f99029460ef8610ef56d12827b21293
LeeHaiLim <hailim1997@gmail.com>
2022-11-26 16:22:16 +0900

    feat(view): implement class output view
    Added methods:
    - print result
    - print result by car
    - print winner
    - print error message
    - print empty line

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,35 @@
+package racingcar.view;
+
+import java.util.List;
+
+public class OutputView {
+    private static final OutputView INSTANCE = new OutputView();
+
+    private OutputView() {
+    }
+
+    public void printResult() {
+        System.out.println(OutputMessage.RESULT.message());
+    }
+
+    public void printResultByCar(String carName, int position) {
+        System.out.printf((OutputMessage.RESULT_BY_CAR.message()),
+                carName, OutputMessage.POSITION_MARK.message().repeat(position));
+    }
+
+    public void printEmptyLine(){
+        System.out.println();
+    }
+
+    public void printWinner(List<String> winners) {
+        System.out.println(OutputMessage.WINNER.message() + String.join(OutputMessage.WINNER_DELIMITER.message(), winners));
+    }
+
+    public void printErrorMessage(String message){
+        System.out.println(message);
+    }
+
+    public static OutputView getInstance() {
+        return INSTANCE;
+    }
+}
