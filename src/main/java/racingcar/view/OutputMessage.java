79512a78d787f261dcfa6ac68712782e692d10ac
LeeHaiLim <hailim1997@gmail.com>
2022-11-26 16:21:10 +0900

    feat(view): add constants to class output message

--- src/main/java/racingcar/view/OutputMessage.java
+++ src/main/java/racingcar/view/OutputMessage.java
@@ -2,10 +2,10 @@ package racingcar.view;
 
 public enum OutputMessage {
     RESULT("실행 결과"),
-    RESULT_BY_CAR("%s : "),
+    RESULT_BY_CAR("%s : %s\n"),
+    POSITION_MARK("-"),
     WINNER("최종 우승자 : "),
-    WINNER_DELIMITER(",")
-    ;
+    WINNER_DELIMITER(", ");
     private final String message;
 
     OutputMessage(String message) {
