eecd1c39578d9798fe89a26d43a14000e2b49889
Limjongho <whdgh9873@icloud.com>
2022-11-26 13:04:53 +0900

    feat(Movement): 멤버별 상태 변수 추가

--- src/main/java/domain/Movement.java
+++ src/main/java/domain/Movement.java
@@ -1,5 +1,14 @@
 package domain;
 
 public enum Movement {
-    MOVE,STOP
+    MOVE(true,1),
+    STOP(false, 0);
+
+    private final boolean canMove;
+    private final int changePosition;
+
+    private Movement(boolean canMove, int changePosition) {
+        this.canMove = canMove;
+        this.changePosition = changePosition;
+    }
 }
