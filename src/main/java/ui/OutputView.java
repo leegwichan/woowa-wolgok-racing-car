e694b6ccfe8ee3aed53fd0a5101393a332870370
Limjongho <whdgh9873@icloud.com>
2022-11-26 16:31:52 +0900

    refactor(OutputView): 클래스 내의 문자열 wrapping

--- src/main/java/ui/OutputView.java
+++ src/main/java/ui/OutputView.java
@@ -5,42 +5,46 @@ import java.util.Map;
 import java.util.Set;
 
 public class OutputView {
+    private static final String EMPTY_LETTER = " ";
+    private static final String SEPARATOR = " : ";
+    private static final String MOVE_EXPRESSION = "-";
+    private static final String WINNER_TEXT = "최종 우승자 :";
+    private static final String COMMA = ",";
 
     public void printResult(Map<String, Integer> namesAndPositions) {
         Set<String> names = namesAndPositions.keySet();
         for (String name : names) {
             System.out.println(getCarResult(namesAndPositions, name));
         }
-        System.out.println(" ");
+        System.out.println(EMPTY_LETTER);
     }
 
     public void printWinner(List<String> winners) {
-        String result = "최종 우승자 :";
-        System.out.println(result + getWinnersResult(winners));
+        System.out.println(WINNER_TEXT + getWinnersResult(winners));
     }
 
     private String getCarResult(Map<String, Integer> namesAndPositions, String name) {
-        return name + " : " + getMoving(namesAndPositions.get(name));
+        return name + SEPARATOR + getMoving(namesAndPositions.get(name));
     }
 
     private String getMoving(int changedPosition) {
-        return "-".repeat(Math.max(0, changedPosition));
+        return MOVE_EXPRESSION.repeat(Math.max(0, changedPosition));
     }
 
     private String getWinnersResult(List<String> winners) {
-        StringBuilder answer = new StringBuilder();
+        StringBuilder winnerResult = new StringBuilder();
         int size = winners.size();
-        makeWinnersResult(winners, answer, size);
-        return answer.toString();
+        makeWinnersResult(winners, winnerResult, size);
+        return winnerResult.toString();
     }
 
-    private void makeWinnersResult(List<String> winners, StringBuilder answer, int size) {
-        for (int i = 0; i < size; i++) {
-            if (i < size - 1) {
-                answer.append(" " + winners.get(i) + ",");
+    private void makeWinnersResult(List<String> winners, StringBuilder answer, int winnersNum) {
+        for (int i = 0; i < winnersNum; i++) {
+            if (i < winnersNum - 1) {
+                answer.append(EMPTY_LETTER + winners.get(i) + COMMA);
                 continue;
             }
-            answer.append(" " + winners.get(i));
+            answer.append(EMPTY_LETTER + winners.get(i));
         }
     }
 }
