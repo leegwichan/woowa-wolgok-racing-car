716aa234facdd9cc9a7cea5598237b00b4458f69
EUNJEONGMUN <munej26@gmail.com>
2022-11-26 15:07:32 +0900

    feat: 자동차 실행 결과를 출력하는 메서드 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,11 @@
+package racingcar.view;
+
+import racingcar.Constant.Message;
+
+public class OutputView {
+
+    public void printRaceState(String raceState) {
+        System.out.println(Message.RESULT_MESSAGE);
+        System.out.println(raceState);
+    }
+}
