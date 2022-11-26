9bf705e49bd50c06741a99be7e8cf3465edfd444
EUNJEONGMUN <munej26@gmail.com>
2022-11-26 15:02:36 +0900

    feat: 전체 자동차의 상태를 출력하기 위해 Cars에 toString 오버라이딩

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import racingcar.Constant.Constant;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,5 +17,13 @@ public class Cars {
     public void moveForward() {
         this.cars.stream()
                 .forEach(car -> car.moveForward());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder raceResult = new StringBuilder();
+        this.cars.stream()
+                .forEach(car -> raceResult.append(car.toString()+ Constant.ENTER));
+        return raceResult.toString();
     }
 }
