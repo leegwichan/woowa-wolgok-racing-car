31fa5ed49f4d6e168257e293c61fd16e674b9b5a
Limjongho <whdgh9873@icloud.com>
2022-11-26 17:02:50 +0900

    refactor: 예외 문구 관리 클래스 추가 및 관련 내용 수정

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static util.ErrorMessage.*;
+
 public class InputView {
 
     private static final int MAXIMUM_NAME_LENGTH = 5;
@@ -32,19 +34,19 @@ public class InputView {
 
     private void isContainingComma(String namesInput) {
         if (!namesInput.contains(",")) {
-            throw new IllegalArgumentException("[ERROR] 각 자동차의 입력은 쉼표(,) 로 구분되어야 합니다.");
+            throw new IllegalArgumentException(SEPARATOR_NOT_EXIST_ERROR_MESSAGE);
         }
     }
 
     private void isContainingBlack(String namesInput) {
         if (namesInput.contains(" ")) {
-            throw new IllegalArgumentException("[ERROR] 자동차의 이름 입력값에는 공백이 포함되지 않아야 합니다.");
+            throw new IllegalArgumentException(CONTAIN_EMPTY_TEXT_ERROR_MESSAGE);
         }
     }
 
     private void isSingleCar(List<String> names) {
         if (names.size() < 2) {
-            throw new IllegalArgumentException("[ERROR] 경주 게임에서 2개 이상의 자동차의 이름이 입력되어야 합니다.");
+            throw new IllegalArgumentException(SINGLE_CAR_NAME_INPUT_ERROR_MESSAGE);
         }
     }
 
@@ -52,7 +54,7 @@ public class InputView {
         boolean isValidNameLength = names.stream()
                 .allMatch(name -> name.length() <= MAXIMUM_NAME_LENGTH);
         if (!isValidNameLength) {
-            throw new IllegalArgumentException("[ERROR] 자동차의 이름은 5자 이하로 입력해야 합니다.");
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
         }
     }
 
@@ -60,13 +62,13 @@ public class InputView {
         try {
             Integer.parseInt(attemptsInput);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자로 입력되어야 합니다.");
+            throw new IllegalArgumentException(NON_NUMERAL_INPUT_ERROR_MESSAGE);
         }
     }
 
     private void isValidAttempts(int attempts) {
         if (attempts < MINIMUM_ATTEMPT) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수는 1 이상의 숫자가 입력되어야 합니다.");
+            throw new IllegalArgumentException(INVALID_ATTEMPT_INPUT_ERROR_MESSAGE);
         }
     }
 }
---
+++ src/main/java/util/ErrorMessage.java
@@ -0,0 +1,16 @@
+package util;
+
+public class ErrorMessage {
+    public static final String NAME_LENGTH_ERROR_MESSAGE =
+            "[ERROR] 자동차의 이름은 5자 이하로 입력해야 합니다.";
+    public static final String NON_NUMERAL_INPUT_ERROR_MESSAGE =
+            "[ERROR] 시도 횟수는 숫자로 입력되어야 합니다.";
+    public static final String INVALID_ATTEMPT_INPUT_ERROR_MESSAGE =
+            "[ERROR] 시도 횟수는 1 이상의 숫자가 입력되어야 합니다.";
+    public static final String SEPARATOR_NOT_EXIST_ERROR_MESSAGE =
+            "[ERROR] 각 자동차의 입력은 쉼표(,) 로 구분되어야 합니다.";
+    public static final String CONTAIN_EMPTY_TEXT_ERROR_MESSAGE =
+            "[ERROR] 자동차의 이름 입력값에는 공백이 포함되지 않아야 합니다.";
+    public static final String SINGLE_CAR_NAME_INPUT_ERROR_MESSAGE =
+            "[ERROR] 경주 게임에서 2개 이상의 자동차의 이름이 입력되어야 합니다.";
+}
