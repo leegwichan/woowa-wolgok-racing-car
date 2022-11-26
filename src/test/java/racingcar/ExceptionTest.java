a1084b3c4b880709a59d5b4af95df313cf499868
Limjongho <whdgh9873@icloud.com>
2022-11-26 15:53:12 +0900

    test: 예외처리 테스트 메서드명 수정

--- src/test/java/racingcar/ExceptionTest.java
+++ src/test/java/racingcar/ExceptionTest.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -15,7 +16,7 @@ public class ExceptionTest extends NsTest {
     @DisplayName("이름 입력에 대한 예외처리 테스트")
     @ValueSource(strings = {" ", "pobi,javajigi", "pobi, javajigi", "pobi"})
     @ParameterizedTest
-    void enterInvalidBridgeLengthTest(String input) {
+    void enterInvalidCarNamesTest(String input) {
         assertSimpleTest(() -> {
             runException(input);
             assertThat(output()).contains(ERROR_MESSAGE);
@@ -25,7 +26,7 @@ public class ExceptionTest extends NsTest {
     @DisplayName("시도 횟수 입력에 대한 예외처리 테스트")
     @ValueSource(strings = {"0", "-1", "d"})
     @ParameterizedTest
-    void enterInvalidMovingBlockTest(String input) {
+    void enterInvalidAttemptsTest(String input) {
         assertSimpleTest(() -> {
             runException("pobi,java", input);
             assertThat(output()).contains(ERROR_MESSAGE);
