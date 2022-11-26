ebed1459d92990a7a7151b493464c581a7713c82
LeeHaiLim <hailim1997@gmail.com>
2022-11-26 16:19:15 +0900

    feat(view): add string constant to input message

--- src/main/java/racingcar/view/InputMessage.java
+++ src/main/java/racingcar/view/InputMessage.java
@@ -1,8 +1,10 @@
 package racingcar.view;
 
 public enum InputMessage {
-    CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    CAR_NAME_DELIMITER(","),
+    CAR_NAME(String.format("경주할 자동차 이름을 입력하세요.(이름은 쉼표(%s) 기준으로 구분)", CAR_NAME_DELIMITER.message())),
     TRIAL_COUNT("시도할 회수는 몇회인가요?");
+
     private final String message;
 
     InputMessage(String message) {
