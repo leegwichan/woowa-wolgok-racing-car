3ba18f1d5f48ccf2d552428c04631e7bddae7613
Limjongho <whdgh9873@icloud.com>
2022-11-26 13:01:18 +0900

    feat(NumberGenerator): 랜덤 숫자 생성 기능 추가

--- src/main/java/util/NumberGenerator.java
+++ src/main/java/util/NumberGenerator.java
@@ -1,8 +1,13 @@
 package util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class NumberGenerator {
 
+    private static final int RANDOM_LOWER_INCLUSIVE = 0;
+    private static final int RANDOM_UPPER_INCLUSIVE = 9;
+
     public static int createRandomNumber() {
-        return 0;
+        return Randoms.pickNumberInRange(RANDOM_LOWER_INCLUSIVE, RANDOM_UPPER_INCLUSIVE);
     }
 }
