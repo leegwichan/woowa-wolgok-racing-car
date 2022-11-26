1bf2eda7870dfedc374896f6aed5698b7bbe6ff2
EUNJEONGMUN <munej26@gmail.com>
2022-11-26 13:45:55 +0900

    feat: 자동차 이름 쉼표로 구분하여 입력받는 메서드 구현

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,22 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.domain.CarName;
+import racingcar.domain.TryCount;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputView {
+    public List<CarName> readCarNames() {
+        try {
+            String carNames = Console.readLine();
+            return Arrays.stream(carNames.split(","))
+                    .map(CarName::new).collect(Collectors.toList());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return readCarNames();
+        }
+    }
+}
