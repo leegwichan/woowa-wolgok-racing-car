5e056e174467f6b2dd62a64a2b08b56f3e9ba705
EUNJEONGMUN <munej26@gmail.com>
2022-11-26 15:40:00 +0900

    feat: 시도 횟수만큼 게임 반복하는 메서드 추가

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -3,12 +3,22 @@ package racingcar.controller;
 import racingcar.domain.Cars;
 import racingcar.domain.TryCount;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class GameController {
     private final static InputView inputview = new InputView();
+    private final static OutputView outputView = new OutputView();
 
     public void startGame() {
         Cars cars = inputview.readCarNames();
         TryCount tryCount = inputview.readTryCount();
+        repeatGame(cars, tryCount.getTryCount());
+    }
+
+    private void repeatGame(Cars cars, int tryCount) {
+        for (int i = 0; i<tryCount; i++) {
+            cars.moveForward();
+            outputView.printRaceState(cars.toString());
+        }
     }
 }
