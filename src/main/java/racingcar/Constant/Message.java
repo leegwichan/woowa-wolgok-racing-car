831b49b4728522e1fced385b502bb6fce433985d
EUNJEONGMUN <munej26@gmail.com>
2022-11-26 15:58:15 +0900

    fix: 최종 우승자 상수 변경

--- src/main/java/racingcar/Constant/Message.java
+++ src/main/java/racingcar/Constant/Message.java
@@ -4,7 +4,7 @@ public enum Message {
     READ_CAR_NAME_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     READ_TRY_COUNT_MESSAGE("시도할 회수는 몇회인가요?"),
     RESULT_MESSAGE("실행 결과"),
-    WINNER_MESSAGE("최종 우승자");
+    WINNER_MESSAGE("최종 우승자 : %s");
 
     private final String message;
 
