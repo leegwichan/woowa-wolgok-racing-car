7700a5f51e644f90f52066b54ea16f191cc4b6cf
LeeHaiLim <hailim1997@gmail.com>
2022-11-26 16:20:01 +0900

    refactor(view): change return type of read car name
    string to string list

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,9 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final InputView INSTANCE = new InputView();
@@ -8,9 +11,11 @@ public class InputView {
     private InputView() {
     }
 
-    public String readCarName() {
+    public List<String> readCarName() {
         System.out.println(InputMessage.CAR_NAME.message());
-        return Console.readLine();
+        return Arrays.stream(Console.readLine().split(InputMessage.CAR_NAME_DELIMITER.message(),-1))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 
     public String readTrialCount() {
