b53cafb82d81e34be3ccbe3a6bbbb6e4d4b57cf7
Limjongho <whdgh9873@icloud.com>
2022-11-26 16:15:17 +0900

    test: 테스트 코드 내 불필요한 import문 삭제

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CarTest {
 
     Car car;
