716c09348b18a22e7ebbb39761d5c10f871c6ffa
LeeHaiLim <hailim1997@gmail.com>
2022-11-26 16:38:33 +0900

    style(domain): reformat lines and spaces

--- src/main/java/racingcar/domain/RandomNumberGenerator.java
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -7,6 +7,6 @@ public class RandomNumberGenerator {
     private static final int NUMBER_RANGE_END = 9;
 
     public int getRandomNumber() {
-        return Randoms.pickNumberInRange(NUMBER_RANGE_START,NUMBER_RANGE_END);
+        return Randoms.pickNumberInRange(NUMBER_RANGE_START, NUMBER_RANGE_END);
     }
 }
