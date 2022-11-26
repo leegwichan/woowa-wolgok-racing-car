72002ffc09996efbdd8ecf2cd7e440c72746f738
LeeHaiLim <hailim1997@gmail.com>
2022-11-26 13:25:45 +0900

    feat(view): implement enum class output message
    Added string constants:
    - result output message
    - result by car output message
    - winner output message
    - winner delimiter

---
+++ src/main/java/racingcar/view/OutputMessage.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+public enum OutputMessage {
+    RESULT("실행 결과"),
+    RESULT_BY_CAR("%s : "),
+    WINNER("최종 우승자 : "),
+    WINNER_DELIMITER(",")
+    ;
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return message;
+    }
+}
