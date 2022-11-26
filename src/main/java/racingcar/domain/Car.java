fc999abb3341b565e75b1ab34837ac962bd2014c
EUNJEONGMUN <munej26@gmail.com>
2022-11-26 15:36:26 +0900

    feat: 자동차 이름을 얻을 수 있는 getName메서드 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -43,4 +43,8 @@ public class Car {
     public String getCarState() {
         return name.getName()+ Constant.DELIMITER+makeCarState();
     }
+
+    public String getCarName() {
+        return name.toString();
+    }
 }
