e4253662ed756326018cdfa7bd83b981ac0640f9
LeeHaiLim <hailim1997@gmail.com>
2022-11-26 16:24:41 +0900

    feat(domain): implement class random number generator
    Added method:
    - get random number

---
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    private static final int NUMBER_RANGE_START = 0;
+    private static final int NUMBER_RANGE_END = 9;
+
+    public int getRandomNumber() {
+        return Randoms.pickNumberInRange(NUMBER_RANGE_START,NUMBER_RANGE_END);
+    }
+}
